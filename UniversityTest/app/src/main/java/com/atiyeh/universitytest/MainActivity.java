17b802360256cc821142b899eed79fda1285f83a
atiyehghm <atiyeh1997@gmail.com>
2020-03-24 18:44:40 +0430

    until android-02 slide 60

--- UniversityTest/app/src/main/java/com/atiyeh/universitytest/MainActivity.java
+++ UniversityTest/app/src/main/java/com/atiyeh/universitytest/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +15,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ThreadSubclass();
+        Button bt0 = findViewById(R.id.bt0);
+        bt0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        getString(R.string.welcome),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //ThreadSubclass();
     }
 
-    void ThreadSubclass(){
+    public void showWelcome(View view) {
+        Toast.makeText(MainActivity.this,
+                getString(R.string.welcome),
+                Toast.LENGTH_LONG).show();
+        Button bt0 = findViewById(R.id.bt0);
+        bt0.setVisibility(View.VISIBLE);
+    }
+
+    /*void ThreadSubclass(){
         Log.i("Universityapp", "ThreadSubclass, Main Thread ]]>>" +
                 " pid: "+ android.os.Process.myPid()+
                 " tid: "+ android.os.Process.myTid()+
@@ -29,5 +50,5 @@ public class MainActivity extends AppCompatActivity {
                 " pid: "+ android.os.Process.myPid()+
                 " tid: "+ android.os.Process.myTid()+
                 " id: " + Thread.currentThread().getId());
-    }
+    }*/
 }
